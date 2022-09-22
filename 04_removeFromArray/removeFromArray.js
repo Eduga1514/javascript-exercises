6d14b734f9e01dc753f9fd4e6de5141ab8374f58
Eduardo Gavazut <eduga.1514@gmail.com>
2022-09-22 14:24:14 -0700

    Solved for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    const result = [];
+    let arrSize = arr.length - 1;
+    let arrIndex = 0;
+    let resultIndex = 0;
 
+    while (arrIndex <= arrSize) {
+        if (targets.includes(arr[arrIndex])) {
+            arrIndex++;
+            continue;
+        }
+
+        result[resultIndex] = arr[arrIndex];
+        arrIndex++;
+        resultIndex++;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
