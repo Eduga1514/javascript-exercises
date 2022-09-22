25c9b19d5787328e2c1a069d1ceb05abb3ccc091
Eduardo Gavazut <eduga.1514@gmail.com>
2022-09-22 15:01:19 -0700

    Solved for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  const result = (temp - 32) * (5/9);
 
+  if (!Number.isInteger(result)) {
+    return Number(result.toFixed(1));
+  }
+
+  return result;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  const result = (temp * (9/5)) + 32;
 
+  if (!Number.isInteger(result)) {
+    return Number(result.toFixed(1));
+  }
+
+  return result;
 };
 
 // Do not edit below this line
