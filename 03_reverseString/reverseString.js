f692e70d5a470f9b96f54617b46f832dcf7e6ace
Eduardo Gavazut <eduga.1514@gmail.com>
2022-09-22 14:03:35 -0700

    Solved for 03_reversedString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    const lastIndex = str.length - 1;
+    let result = '';
 
+    for (let i = lastIndex; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result
 };
 
 // Do not edit below this line
